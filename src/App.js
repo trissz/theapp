f70429ed170674e9949a597bd492a43bac7c5fc3
csoko99 <laczkolali77@gmail.com>
2022-09-28 13:04:51 +0200

    first commit try

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Szopd le apám!!
         </p>
         <a
           className="App-link"
