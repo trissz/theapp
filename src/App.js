3f525bed0480b773806ce818da055401045d8abc
trissz <triszten25@gmail.com>
2022-09-28 21:29:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,24 +30,7 @@ export default function ButtonAppBar() {
       </AppBar>
     </Box>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
 
   );
 }
